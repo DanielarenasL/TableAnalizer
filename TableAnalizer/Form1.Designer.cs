ef93b87e86c89f8e8b51f4cbeafd6557ee1e7501
DanielarenasL <daniel2004al14@gmail.com>
2025-01-24 07:20:53 -0500

    unos cambios

--- TableAnalizer/Form1.Designer.cs
+++ TableAnalizer/Form1.Designer.cs
@@ -28,59 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.printDialog1 = new System.Windows.Forms.PrintDialog();
-            this.printDialog2 = new System.Windows.Forms.PrintDialog();
-            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
-            this.label1 = new System.Windows.Forms.Label();
+            this.SelectDocument = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // button1
+            // SelectDocument
             // 
-            this.button1.Location = new System.Drawing.Point(106, 161);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(262, 109);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.SelectDocument.AutoSize = true;
+            this.SelectDocument.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.SelectDocument.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SelectDocument.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectDocument.Location = new System.Drawing.Point(163, 107);
+            this.SelectDocument.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.SelectDocument.Name = "SelectDocument";
+            this.SelectDocument.Size = new System.Drawing.Size(386, 202);
+            this.SelectDocument.TabIndex = 0;
+            this.SelectDocument.Text = "Seleccione el documento";
+            this.SelectDocument.UseMnemonic = false;
+            this.SelectDocument.UseVisualStyleBackColor = false;
+            this.SelectDocument.Click += new System.EventHandler(this.SelectDocument_Click);
             // 
-            // printDialog1
+            // openFileDialog1
             // 
-            this.printDialog1.UseEXDialog = true;
-            // 
-            // printDialog2
-            // 
-            this.printDialog2.UseEXDialog = true;
-            // 
-            // printPreviewDialog1
-            // 
-            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
-            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
-            this.printPreviewDialog1.Enabled = true;
-            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
-            this.printPreviewDialog1.Name = "printPreviewDialog1";
-            this.printPreviewDialog1.Visible = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(237, 81);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 434);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(726, 419);
+            this.Controls.Add(this.SelectDocument);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -90,11 +70,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PrintDialog printDialog1;
-        private System.Windows.Forms.PrintDialog printDialog2;
-        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SelectDocument;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- TableAnalizer/Form1.cs
+++ TableAnalizer/Form1.cs
@@ -7,28 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OfficeOpenXml; // Agrega la referencia a EPPlus
 
 namespace TableAnalizer
 {
     public partial class Form1 : Form
     {
+
+
+
         public Form1()
         {
+            // Establece el contexto de licencia a "NonCommercial" (no comercial)
+            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
             InitializeComponent();
+            openFileDialog1 = new OpenFileDialog();
+
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
+        private void SelectDocument_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Archivos de Excel (*.xlsx)|*.xlsx";
+
+            // Muestra el diálogo de apertura de archivo
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Obtiene la ruta del archivo seleccionado
+                string filePath = openFileDialog1.FileName;
+
+                // Llama a un método para cargar los datos de Excel
+                var dataTable = LoadExcelData(filePath);
+
+                Form2 form2 = new Form2(dataTable);
+                form2.Show();
+                // Aquí puedes agregar código adicional para leer o procesar el archivo
+
+            }
         }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private System.Data.DataTable LoadExcelData(string filePath)
         {
+            var dataTable = new System.Data.DataTable();
 
+            // Abre el archivo Excel y lee las celdas
+            using (var package = new ExcelPackage(new System.IO.FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0]; // Lee la primera hoja
+                var startRow = worksheet.Dimension.Start.Row;
+                var endRow = worksheet.Dimension.End.Row;
+                var startCol = worksheet.Dimension.Start.Column;
+                var endCol = worksheet.Dimension.End.Column;
+
+                // Crea las columnas del DataTable
+                for (int col = startCol; col <= endCol; col++)
+                {
+                    dataTable.Columns.Add(worksheet.Cells[1, col].Text); // Títulos de las columnas
+                }
+
+                // Rellena las filas del DataTable con los datos del Excel
+                for (int row = startRow + 1; row <= endRow; row++)
+                {
+                    var rowData = dataTable.NewRow();
+                    for (int col = startCol; col <= endCol; col++)
+                    {
+                        rowData[col - 1] = worksheet.Cells[row, col].Text;
+                    }
+                    dataTable.Rows.Add(rowData);
+                }
+            }
+
+            return dataTable;
         }
     }
 }
