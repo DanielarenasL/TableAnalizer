02445bb72438dfd0a4dca283d05b25401591738e
DanielarenasL <daniel2004al14@gmail.com>
2025-01-24 09:39:46 -0500

    con porcentajes mamalones

--- TableAnalizer/Form1.Designer.cs
+++ TableAnalizer/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.SelectDocument = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // SelectDocument
@@ -38,10 +42,10 @@
             this.SelectDocument.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.SelectDocument.Cursor = System.Windows.Forms.Cursors.Hand;
             this.SelectDocument.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SelectDocument.Location = new System.Drawing.Point(163, 107);
+            this.SelectDocument.Location = new System.Drawing.Point(464, 627);
             this.SelectDocument.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.SelectDocument.Name = "SelectDocument";
-            this.SelectDocument.Size = new System.Drawing.Size(386, 202);
+            this.SelectDocument.Size = new System.Drawing.Size(337, 159);
             this.SelectDocument.TabIndex = 0;
             this.SelectDocument.Text = "Seleccione el documento";
             this.SelectDocument.UseMnemonic = false;
@@ -52,17 +56,50 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-3, -8);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1026, 573);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1029, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 39);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Total";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1029, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Total";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 419);
+            this.ClientSize = new System.Drawing.Size(1216, 875);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.SelectDocument);
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +109,9 @@
 
         private System.Windows.Forms.Button SelectDocument;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- TableAnalizer/Form1.cs
+++ TableAnalizer/Form1.cs
@@ -13,6 +13,9 @@ namespace TableAnalizer
 {
     public partial class Form1 : Form
     {
+        private int totalLotes = 0;
+        private int lotesPassed = 0;
+        private int lotesFailed = 0;
 
 
 
@@ -22,12 +25,39 @@ namespace TableAnalizer
             OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
             InitializeComponent();
             openFileDialog1 = new OpenFileDialog();
+            this.WindowState = FormWindowState.Maximized;
 
-            
+            // Suscribirse al evento de formato de celda
+            dataGridView1.CellFormatting += DataGridView1_CellFormatting;
+
+
+            View();
+
+        }
+
+        private void View()
+        {
+            if(SelectDocument.Visible == Visible)
+            {
+                label1.Visible = false;
+                label2.Visible = false;
+            }
+            else
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Cambia el estilo de las cabeceras de columna a negrita
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font(dataGridView1.Font, FontStyle.Bold);
+
+            // Opcionalmente, también puedes cambiar el color de fondo o el color del texto de las cabeceras:
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Gray;
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
         }
 
 
@@ -44,9 +74,10 @@ namespace TableAnalizer
                 // Llama a un método para cargar los datos de Excel
                 var dataTable = LoadExcelData(filePath);
 
-                Form2 form2 = new Form2(dataTable);
-                form2.Show();
-                // Aquí puedes agregar código adicional para leer o procesar el archivo
+                // Asigna el DataTable al DataGridView
+                dataGridView1.DataSource = dataTable;
+
+                CountBatch(dataTable);
 
             }
         }
@@ -80,8 +111,68 @@ namespace TableAnalizer
                     dataTable.Rows.Add(rowData);
                 }
             }
+            SelectDocument.Visible = false;
+            SelectDocument.Enabled = false;
+            View();
+
+
 
             return dataTable;
         }
+        private void DataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            // Verifica que estamos en la columna correcta
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Batch Status")
+            {
+                // Verifica si el valor de la celda no es nulo y es un número
+                if (e.Value != null && e.Value is string valor)
+                {
+                    // Si el valor es mayor a 100, cambia el fondo de la celda a rojo
+                    if (valor == "FAILED")
+                    {
+                        dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.Red;
+                        dataGridView1.Rows[e.RowIndex].DefaultCellStyle.ForeColor = Color.White;
+                    }
+                    else
+                    {
+                        e.CellStyle.BackColor = Color.White; // Fondo blanco por defecto
+                        e.CellStyle.ForeColor = Color.Black; // Texto negro por defecto
+                    }
+                }
+            }
+        }
+
+        private void CountBatch(DataTable dataTable)
+        {
+            totalLotes = dataTable.Rows.Count;
+            lotesFailed = 0;
+            lotesPassed = 0;
+            foreach (DataRow row in dataTable.Rows) 
+            {
+                string batchValue = row["Batch Status"].ToString();
+
+                if (batchValue == "FAILED")
+                {
+                    lotesFailed++;
+                }
+                else if(batchValue == "PASSED"){
+                    lotesPassed++;
+                }
+            }
+
+
+            double passedPercentage = (100.0 * lotesPassed) / totalLotes;
+            double failedPercentage = (100.0 * lotesFailed) / totalLotes;
+
+            label1.Text = $"Total: {totalLotes} \n\nPassed: {lotesPassed} \n\nFailed: {lotesFailed}";
+            label2.Text = $"Passed Percentage: \n{passedPercentage:F2}% \n\nFailed Percentage: \n{failedPercentage:F2}%";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
     }
 }
